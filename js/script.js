56718ef87d2818c4f906bc3358cb1603f3475c39
Asif Ansari <iamasif11996@gmail.com>
2025-05-10 12:24:28 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,19 +37,19 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/img/cloud.png";
+            weather_img.src = "./img/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/img/clear.png";
+            weather_img.src = "./img/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/img/rain.png";
+            weather_img.src = "./img/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/img/mist.png";
+            weather_img.src = "./img/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/img/snow.png";
+            weather_img.src = "./img/snow.png";
             break;
 
     }
